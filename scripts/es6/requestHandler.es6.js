456ccee28966531818e2dc462241d9491efbfbf2
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-06-24 18:56:36 +0200

    last

--- scripts/es6/requestHandler.es6.js
+++ scripts/es6/requestHandler.es6.js
@@ -1,6 +1,16 @@
 'use strict';
 class requestHandler {
-    fetch(url, callback = (req, res) =>{ console.log('default cb', res);} ) {
+    constructor() {
+        this.contentMap = new Map();
+    }
+
+    createMap(mapData="") {
+        this.contentMap.set('text/plain', ['txt', 'rtf']);
+        this.contentMap.set('text/json', ['json']);
+        this.contentMap.set('application/xml', ['xml']);
+    }   
+    
+    fetch(url, callback = (req, res) => { console.log('default cb', res);} ) {
         let req = new XMLHttpRequest();
         req.onreadystatechange = () => {
             if (req.readyState === XMLHttpRequest.DONE) {
@@ -22,34 +32,15 @@ class requestHandler {
             return this.indexOf(x) >= 0;
         }
         req.open('GET', url);
-        //new request for map json
-        let mapReq = new XMLHttpRequest();
-        mapReq.onreadystatechange = () => {
-            if (mapReq === XMLHttpRequest.DONE) {
-                if ( mapReq.status === 200) {
-                    //tu wiemy ze chcemy Jsona to od razu go obslugujemy
-                    let mapRes = JSON.parse(mapRes.responseText);
-                    //callback dla mapy,
-                } else {
-                    console.log('map error');
-                }
-            }
-        }
-        mapReq.open('GET', 'maps/maps.json');
-        mapReq.send(null);
+        req.send(null);   
+        createMap(); 
                         
-        let contentMap = new Map();
-        contentMap.set('text/plain', ['txt', 'rtf']);
-        contentMap.set('text/json', ['json']);
-        contentMap.set('application/xml', ['xml']);
         let fileType = url.split('.').pop();
-        for (let [key,value] of contentMap) {
+        for (let [key,value] of this.contentMap) {
             if (value.includes(fileType)){
                 req.setRequestHeader('Content-Type', key);
             }
         }
-         
-        req.send(null);
     }
         
 }
