a8e05c7aeedff2fc57b13d62652b787cebe2c43f
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-06-26 20:46:16 +0200

    global fetch

--- app/scripts/main.es6.js
+++ app/scripts/main.es6.js
@@ -2,23 +2,25 @@
 
 import {} from './ecma/es7/Array.js';
 import RequestHandler from './modules/RequestHandler.es6.js';
-
-// global (JSON dependency)
-window.RequestHandler = RequestHandler;
-
+// global (JSON dependency) deprecated
+window.mimeTypes = {
+    "text/plain": ["txt", "rtf"],
+    "text/json": ["json"],
+    "application/xml": ["xml"]
+};
 window.addEventListener('load', () => {
 
-    let xhr = new RequestHandler();
+    //let xhr = new RequestHandler();
 
-    xhr.fetch('./data/data1.json').then( (response) => {
+    fetch('./data/data1.json').then( (response) => {
         console.info('[DONE] ./data/data1.json');
     }, () => {
         console.error('[FAILED] ./data/data1.json');
     });
 
-    let xhr2 = new RequestHandler();
+    //let xhr2 = new RequestHandler();
 
-    xhr2.fetch('./data/data2.xml').then( (response) => {
+    fetch('./data/data2.xml').then( (response) => {
         console.info('[DONE] ./data/data2.xml');
     }, () => {
         console.error('[FAILED] ./data/data2.xml');
