0b7750bd791d1168ba7bf4c9887162a053468cda
Bartłomiej Sobczuk <bartlomiej.sobczuk@gmail.com>
2015-06-26 21:06:19 +0200

    no REQHandler

--- app/scripts/main.es6.js
+++ app/scripts/main.es6.js
@@ -1,26 +1,36 @@
 'use strict';
 
-import {} from './ecma/es7/Array.js';
-import RequestHandler from './modules/RequestHandler.es6.js';
-// global (JSON dependency) deprecated
-window.mimeTypes = {
+window.mimeType = {
     "text/plain": ["txt", "rtf"],
     "text/json": ["json"],
     "application/xml": ["xml"]
 };
 window.addEventListener('load', () => {
+    let _setHeader = (url) => {
+        let fileType = url.split('.').pop();
+        let mimeTypesMap = new Map();
+        let header = new Headers();  
+        for (let mimeType in mimeType) {
+            if (mimeType.hasOwnProperty(mimeType)) {
+                mimeTypesMap.set(mimeType, mimeType[mimeType]);
+            }
+        }
 
-    //let xhr = new RequestHandler();
-
-    fetch('./data/data1.json').then( (response) => {
+        for (let [key, value] of mimeTypesMap) {
+            if (value.includes(fileType)) {
+                header.append('Content-Type', key);
+            }
+        }
+        return header;
+    }
+    
+    fetch('./data/data1.json', {header: _setHeader('./data/data1.json')}).then((response) => {
         console.info('[DONE] ./data/data1.json');
     }, () => {
         console.error('[FAILED] ./data/data1.json');
     });
 
-    //let xhr2 = new RequestHandler();
-
-    fetch('./data/data2.xml').then( (response) => {
+    fetch('./data/data2.xml', {header: _setHeader('./data/data2.xml')}).then((response) => {
         console.info('[DONE] ./data/data2.xml');
     }, () => {
         console.error('[FAILED] ./data/data2.xml');
